2ffc24a5312b8a7e8f89b05d3d6d68a0d7af3c0f
Christian.Hoeller <ch.hoeller14@gmail.com>
2024-06-08 18:00:50 +0200

    feat: add prettier config file and change ws.types

--- backend/src/model/game.model.ts
+++ backend/src/model/game.model.ts
@@ -1,4 +1,4 @@
-import { UserDocument } from "./user.model"
+import { UserDocument } from "./user.model";
 
 export type Move = {
     player: UserDocument["id"];
@@ -6,14 +6,13 @@ export type Move = {
     // rfc3339
     timestamp: string;
 
-    field_idx: [number, number];
-    cell_idx: [number, number];
-}
+    position: [number, number];
+};
 
 export type Game = {
-    game_id: string, // generated uuid v4
-    player_x: UserDocument["id"], // objectid
-    player_o: UserDocument["id"], // objectid
-    moves: Array<Move>,
-    winner?: string, // objectid
-}
+    game_id: string; // generated uuid v4
+    player_x: UserDocument["id"]; // objectid
+    player_o: UserDocument["id"]; // objectid
+    moves: Array<Move>;
+    winner?: string; // objectid
+};
--- backend/src/types/ws.types.ts
+++ backend/src/types/ws.types.ts
@@ -4,32 +4,31 @@ import { UserDocument } from "../model/user.model";
 export type WSPlayerInfo = {
     user_id: UserDocument["id"];
     username: UserDocument["username"];
-    
+
     points: number;
-}
+};
 
 export type WSGame = {
     game_id: string;
     player_x: WSPlayerInfo;
     player_y: WSPlayerInfo;
-    moves: Array<Move>,
+    moves: Array<Move>;
 
     // the player that is currently playing - next move swaps the two letters
     curr: "x" | "o";
 
     // the field that is currently being played - chosen randomly at the start
     active_field: [number, number];
-}
+};
 
 export type WSGameFinishedEvent = {
     game_id: string;
     player: WSPlayerInfo;
-}
+};
 
 export type WSLobby = {
-    game_id: string;    // generated uuid v4
+    game_id: string; // generated uuid v4
     name: string;
-    password?: string;
     created: Date;
-    players: Array<UserDocument["id"]>,
-}
+    players: Array<UserDocument["id"]>;
+};
