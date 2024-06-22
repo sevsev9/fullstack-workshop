f68a7a905d4a6a30db4ad9970233b5efd13088d9
Sev <sevm@pm.me>
2024-06-22 19:34:06 +0200

    fix: was retarded

--- backend/src/middleware/deserializeUser.ts
+++ backend/src/middleware/deserializeUser.ts
@@ -20,8 +20,8 @@ export async function deserializeUser(req: Request, res: Response, next: NextFun
 
   const { valid, expired, decoded, error } = verifyJwt(accessToken);
 
-  // if the token is invalid but also not expired
-  if (!valid && !expired) {
+  // if the token is invalid
+  if (!valid) {
     logger.warn(`Access token invalid: ${error} | '${accessToken}'`);
     res.locals.error = "Access token invalid."
     return next();
@@ -30,27 +30,16 @@ export async function deserializeUser(req: Request, res: Response, next: NextFun
   if (decoded) {
     logger.debug(`{Deserialize User} - Session ID from decoded JWT: ${decoded.sessionId} - valid: ${!expired} - url: ${req.url}`);
     try {
-      if (expired) {
-        /**
-         * If expired, the user gives us an access token that has expired,
-         * this does NOT mean we should invalidate the whole session
-         * but rather prompt the user to call the refresh endpoint to aquire a new access token
-         * 
-         * the session should only be invalidated if the user comes with an expired refresh token,
-         * which is to be done in the /auth/refresh handler
-         */
-
-        if (req.url !== "/api/auth/logout" && req.url !== "/api/auth/refresh" && req.url !== "/api/healthcheck") {
-          return res.status(401).json({ error: 'Access token expired, please refresh your session' });
-        } else {
-          logger.debug(`{Deserialize User} - Let expired access token pass to '${req.url}' - SessionId: ${decoded.sessionId}`);
-        }
+      
+      if (!expired) {
+        // Continue with valid decoded information
+        res.locals.user = decoded;
+        logger.debug(`{Deserialize User} - User ${decoded._id} deserialized from JWT`);
+      } else {
+        // Token expired
+        res.locals.error = "Access token expired.";
       }
-
-      // Continue with valid decoded information
-      res.locals.user = decoded;
-      logger.debug(`{Deserialize User} - User ${decoded._id} deserialized from JWT`);
-
+      
       return next();
     } catch (error) {
       logger.error("Error processing JWT: " + (error as Error).message);
