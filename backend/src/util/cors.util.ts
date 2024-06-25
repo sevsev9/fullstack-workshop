f15dfb2cfcbaa7769b42fd1262819101ab857d74
Christian.Hoeller <ch.hoeller14@gmail.com>
2024-06-25 19:42:55 +0200

    feat: update cors to include frontenUrl

--- backend/src/util/cors.util.ts
+++ backend/src/util/cors.util.ts
@@ -2,5 +2,5 @@ import cors from "cors";
 
 export default cors({
     credentials: true,
-    origin: [`http://localhost:${process.env.PORT}`],
+    origin: [`http://localhost:${process.env.PORT}`, process.env.FRONTEND_URL!],
 });
